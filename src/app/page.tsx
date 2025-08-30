d393380cb91224091d6d6790950d111eac760879
Boula-Ayman <boulaaymanzaher@gmail.com>
2025-08-30 16:16:59 +0300

    carousel-mainslider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
 import React from "react";
-
+import Header from "../components/header/header";
+import Footer from "../components/footer/footer";
+import MainSlider from "../components/Caruosel/MainSlider";
 const page = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+      <MainSlider />
+      <Footer />
+    </div>
+  );
 };
 
 export default page;
