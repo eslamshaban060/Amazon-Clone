c3c4ac85867f7174631ecbe6be3314aefac9d696
iamali-stack <icyalikhaled2013@gmail.com>
2025-08-31 12:28:55 +0300

    add Member on Team Roles

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FaSearch, FaMapMarkerAlt, FaGlobe, FaUser, FaShoppingCart, FaBars, FaChevronDown } from "react-icons/fa";
+import { FaSearch, FaMapMarkerAlt, FaShoppingCart, FaChevronDown } from "react-icons/fa";
 import { Menu, ChevronDown } from "lucide-react";
 import Image from "next/image";
 
