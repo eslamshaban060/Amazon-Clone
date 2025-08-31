1b4f3b31372c31b38e1650a6ba486422fdf35a80
Boula-Ayman <boulaaymanzaher@gmail.com>
2025-08-31 15:13:34 +0300

    the smaller slider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,15 @@ import React from "react";
 import Header from "../components/header/header";
 import Footer from "../components/footer/footer";
 import MainSlider from "../components/Caruosel/MainSlider";
+import FirstSlider from "../app/fristSlider/page";
+import SecondtSlider from "../app/secondSlider/page";
 const page = () => {
   return (
     <div>
       <Header />
       <MainSlider />
+      <FirstSlider />
+      <SecondtSlider />
       <Footer />
     </div>
   );
