c0a91b0edb1f10ebdf054ea38c9378de390a93bb
eslamshaban060 <eslamshaban060@gmail.com>
2025-09-02 13:20:53 +0200

    every thing is up to date in the all branches

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,38 @@ import Footer from "../components/footer/footer";
 import MainSlider from "../components/Caruosel/MainSlider";
 import FirstSlider from "../app/fristSlider/page";
 import SecondtSlider from "../app/secondSlider/page";
+import CardsRow from "@/components/homeCards/row";
 const page = () => {
   return (
     <div>
-      <Header />
-      <MainSlider />
-      <FirstSlider />
-      <SecondtSlider />
-      <Footer />
+      {/* header  */}
+      <div>
+        <Header />
+      </div>
+
+      {/* hero slider  */}
+      <main>
+        <MainSlider />
+      </main>
+
+      {/* top cards  */}
+      <section>
+        <CardsRow />
+      </section>
+      {/* first slider   */}
+      <section>
+        <FirstSlider />
+      </section>
+
+      {/* secound section  */}
+      <section>
+        <SecondtSlider />
+      </section>
+
+      {/* footer section  */}
+      <footer>
+        <Footer />
+      </footer>
     </div>
   );
 };
