b4704c19ce91352eb4aedec6f59596a144d0c304
eslamshaban060 <eslamshaban060@gmail.com>
2025-09-08 16:25:26 +0200

    clean main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,7 @@
-import CardsRow from "@/components/homeCards/row";
-import FirstSlider from "@/app/fristSlider/page";
-import SecondtSlider from "@/app/secondSlider/page";
-import MainSlider from "@/components/Caruosel/MainSlider";
+import React from "react";
+
 const page = () => {
-  return (
-    <div className="bg-[var(--bg)]">
-      {/* <Header /> */}
-      <main>
-        {" "}
-        {/* New: Main content area */}
-        <section>
-          {" "}
-          {/*  MainSlider is a distinct section */}
-          <MainSlider />
-        </section>
-        <section>
-          {" "}
-          {/* Existing: Cards section */}
-          <CardsRow />
-        </section>
-        <section>
-          {" "}
-          {/*   FirstSlider is a distinct section */}
-          <FirstSlider />
-        </section>
-        <section>
-          {" "}
-          {/*  SecondtSlider is a distinct section */}
-          <SecondtSlider />
-        </section>
-      </main>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default page;
